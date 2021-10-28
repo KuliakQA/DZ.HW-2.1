5cd6b4d3476899929e35b3b34910e8f854821461
KuliakQA <culeacvadim@gmail.com>
2021-10-28 17:11:54 +0200

    add result

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,8 @@
 public class Main {
+    public static void main(String[] args) {
+        int tiket = 2450;  // стоимость билета
+        int percent = 20;  // процент за 1 милю
+        int milli = tiket / percent;  // расчет миль
+        System.out.println(milli);
+    }
 }
